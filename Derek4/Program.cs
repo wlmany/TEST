e0e9abf46cf049f5ef3eea818dcf5c62515a472c
Wang Lu <e0283992@u.nus.edu>
2018-02-25 17:42:19 +0800

    Modified derek4.csproj

--- Derek4/Program.cs
+++ Derek4/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace Derek4
 {
-	class Program
+	class Animal
 	{
 		static void Main(string[] args)
 		{
+		}
+	}
+	class Cat:Animal
+	{
+		public void q()
+		{ }
+	}
+	class test
+	{
+		public static void Main()
+		{
+			Animal v = new Animal();
+			Cat w = new Cat();
+			Animal v2 = w;
+			Cat w2 = (Cat)v2;//cat can = animal--> cat's area widening;but animal can't= cat.--->will narrow.
+			w2.q();//can't v2.q;because q is a method of animal.
 		}
 	}
 }
